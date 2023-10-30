bd0571da839addb1b13af4d2448dbc9400d1fb12
Tyler Justus <tylerjustus@zudy.com>
2023-10-30 17:25:09 -0400

    feat: reservation form

--- src/Data/User.cs
+++ src/Data/User.cs
@@ -2,12 +2,16 @@
 {
     public class User
     {
-        public DateTime? Date { get; set; }
+        public DateTime? Date { get; set; } = DateTime.Today;
 
-        public TimeSpan? From { get; set; }
+        public TimeSpan? From { get; set; } = DateTime.Now.TimeOfDay;
 
-        public TimeSpan? To { get; set; }
+        public TimeSpan? To { get; set; } = DateTime.Now.TimeOfDay.Add(new TimeSpan(01, 00, 00));
 
-        public string? Repeats { get; set; }
+        public string? Repeats { get; set; } = "Does not repeat";
+
+        public string? ParkingArea { get; set; }
+
+        public string? ParkingLot { get; set; }
     }
 }
