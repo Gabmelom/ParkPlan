247bc917189b428a3b32f0ec852648ddde4ae293
Tyler Justus <tylerjustus@zudy.com>
2023-10-31 15:13:49 -0400

    feat: billing page

--- src/Data/User.cs
+++ src/Data/User.cs
@@ -1,5 +1,7 @@
 ﻿namespace ParkPlan.Data
 {
+    // Stores data for a user
+    // Default values are used to simulate an existing profile
     public class User
     {
         public DateTime? Date { get; set; } = DateTime.Today;
@@ -13,5 +15,24 @@
         public string? ParkingArea { get; set; }
 
         public string? ParkingLot { get; set; }
+
+        public string? FirstName { get; set; } = "Dennis";
+
+        public string? LastName{ get; set; } = "Lacasse";
+
+        public string? BillingAddress1 { get; set; } = "222 Nepean St";
+
+        public string? BillingAddress2 { get; set; }
+
+        public string? Country { get; set; } = "Canada";
+
+        public string? State { get; set; } = "Ontario";
+
+        public string? City { get; set; } = "Ottawa";
+
+        public string? Postal { get; set; } = "K2P0B8";
+
+        public string? Phone { get; set; } = "613-123-4567";
+
     }
 }
